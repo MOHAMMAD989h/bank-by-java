54395c35f889f57d0b810059cd35c1637d0e95fa
meh24284 <mtb136smeh85q@gmail.com>
2025-03-26 13:43:50 -0700

    opening Account

--- src/main/java/com/example/bank/account.java
+++ src/main/java/com/example/bank/account.java
@@ -1,17 +1,29 @@
 package com.example.bank;
 
+import javafx.animation.TranslateTransition;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
+
 import java.net.URL;
 
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class account implements Initializable {
+    @FXML
+    private MenuItem sakht;
+    @FXML
+    private MenuItem Introduce;
+    @FXML
+    private AnchorPane side_form;
+    @FXML
+    private  MenuButton MenuButton;
     @FXML
     public ComboBox<String> com1;
     @FXML
@@ -31,5 +43,29 @@ public class account implements Initializable {
             }
         });*/
 
+    }
+
+    public void switchForm(ActionEvent event) {
+        TranslateTransition slider = new TranslateTransition();
+        if (event.getSource() == sakht) {
+            slider.setNode(side_form);
+            slider.setToX(300);
+            slider.setDuration(Duration.seconds(.5));
+
+            slider.setOnFinished((event1) -> {
+                MenuButton.setVisible(true);
+            });
+
+            slider.play();
+        } else if (event.getSource() == Introduce) {
+            slider.setNode(side_form);
+            slider.setToX(0);
+            slider.setDuration(Duration.seconds(.5));
+
+            slider.setOnFinished((event1) -> {
+                MenuButton.setVisible(true);
+            });
+            slider.play();
+        }
     }
 }
