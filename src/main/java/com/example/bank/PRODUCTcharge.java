d1a86d50d25c57c1fc6e0b0eb63db6f4b9020821
Mohammad Asadpour <asadpour808@gmail.com>
2025-03-27 11:07:19 +0330

    5 commit

---
+++ src/main/java/com/example/bank/PRODUCTcharge.java
@@ -0,0 +1,14 @@
+package com.example.bank;
+
+public class PRODUCTcharge {
+    String price;
+    public PRODUCTcharge(String price) {
+        this.price = price;
+    }
+    public String getPrice() {
+        return price;
+    }
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
